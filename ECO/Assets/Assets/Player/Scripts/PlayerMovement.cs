3c8a47cfe2d4209cb192db90c5498d76ae152cf8
Alex <101017492+faelion@users.noreply.github.com>
2023-06-17 17:23:05 +0200

    Player Jump

--- ECO/Assets/Assets/Player/Scripts/PlayerMovement.cs
+++ ECO/Assets/Assets/Player/Scripts/PlayerMovement.cs
@@ -5,6 +5,12 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [Header("KeyBinds")]
+    [SerializeField] private KeyCode jumpKey = KeyCode.Space;
+
+    [Header("Drag")]
+    float groundDrag = 6f;
+    float airDrag = 1.5f;
 
     [Header("Movement")]
 
@@ -14,6 +20,8 @@ public class PlayerMovement : MonoBehaviour
     float horizontalMovement;
     float verticalMovement;
     [SerializeField] float dashForce = 1500;
+    [SerializeField] float jumpForce = 2.5f;
+    [SerializeField] float airMovMultiplier = 0.2f;
 
     Vector3 moveDirection;
     Vector3 dashForceVector;
@@ -25,60 +33,37 @@ public class PlayerMovement : MonoBehaviour
    
 
     private float lastMovement;
+
+    private bool isGrounded;
+
+    private float playerHeight;
+    private CapsuleCollider playerCollider;
     private void Start()
     {
         player = GetComponent<Rigidbody>();
         player.freezeRotation = true;
+        playerCollider = GetComponent<CapsuleCollider>();
     }
     private void Update()
     {
+        isGrounded = Physics.Raycast(transform.position, Vector3.down, playerCollider.height / 2 + 0.1f);
+        if (Input.GetKeyDown(jumpKey) && isGrounded)
+        {
+            jump();
+        }
+
+        ControlDrag();
         PlayerInput();
-        
-      
-        
     }
 
     private void PlayerInput()
     {
         horizontalMovement = Input.GetAxisRaw("Horizontal");
-        verticalMovement = 0;
-        dashForceVector = transform.right * dashForce * 100;
 
-        if (horizontalMovement !=0)
-        {
-            lastMovement = horizontalMovement;
-        }
-        if (Input.GetMouseButtonDown(1))
-        {
-            if (horizontalMovement >= 1)
-            {
-                lastMovement = horizontalMovement;
-                player.AddForce(dashForceVector, ForceMode.Impulse);
-            }
-             else if (horizontalMovement <= -1)
-            {
-                lastMovement = horizontalMovement;
-
-                player.AddForce(-dashForceVector, ForceMode.Impulse);
-            }
-            else
-            {
-                if (lastMovement >= 1)
-                {
-                    player.AddForce(dashForceVector, ForceMode.Impulse);
-
-                }
-                else
-                {
-                    player.AddForce(-dashForceVector, ForceMode.Impulse);
-
-                }
-            }
-        }
+        Dash();
         
-
         //Se mueve a donde mira
-        moveDirection = transform.forward * verticalMovement + transform.right * horizontalMovement;
+        moveDirection = transform.right * horizontalMovement;
     }
 
     private void FixedUpdate()
@@ -88,16 +73,80 @@ public class PlayerMovement : MonoBehaviour
 
     private void PlayerMove()
     {
-
-        if (Input.GetKey(runKey))
+        if(isGrounded)
         {
-            player.AddForce(moveDirection.normalized * movementSpeed * movementMultiplyer * 2f, ForceMode.Acceleration);
+            if (Input.GetKey(runKey))
+            {
+                player.AddForce(moveDirection.normalized * movementSpeed * movementMultiplyer * 2f, ForceMode.Acceleration);
+            }
+            else
+            {
+                player.AddForce(moveDirection.normalized * movementSpeed * movementMultiplyer, ForceMode.Acceleration);
+            }
         }
         else
         {
-            player.AddForce(moveDirection.normalized * movementSpeed * movementMultiplyer, ForceMode.Acceleration);
+            if (Input.GetKey(runKey))
+            {
+                player.AddForce(moveDirection.normalized * movementSpeed * movementMultiplyer * 2f * airMovMultiplier, ForceMode.Acceleration);
+            }
+            else
+            {
+                player.AddForce(moveDirection.normalized * movementSpeed * movementMultiplyer * airMovMultiplier, ForceMode.Acceleration);
+            }
+        }
+    }
+
+    private void jump()
+    {
+        player.AddForce(transform.up * jumpForce * 1000f, ForceMode.Impulse);
+    }
+
+    private void ControlDrag()
+    {
+        if(isGrounded)
+        {
+            player.drag = groundDrag;
+        }
+        else
+        {
+            player.drag = airDrag;
+        }
+    }
+
+    private void Dash()
+    {
+        dashForceVector = transform.right * dashForce * 100;
+
+        if (horizontalMovement != 0)
+        {
+            lastMovement = horizontalMovement;
         }
 
-        
+        if (Input.GetMouseButtonDown(1))
+        {
+            if (lastMovement <= -1)
+            {
+                if (isGrounded)
+                {
+                    player.AddForce(-dashForceVector, ForceMode.Impulse);
+                }
+                else
+                {
+                    player.AddForce(-dashForceVector * airMovMultiplier * 2.2f, ForceMode.Impulse);
+                }
+            }
+            else
+            {
+                if (isGrounded)
+                {
+                    player.AddForce(dashForceVector, ForceMode.Impulse);
+                }
+                else
+                {
+                    player.AddForce(dashForceVector * airMovMultiplier * 2.2f, ForceMode.Impulse);
+                }
+            }
+        }
     }
 }
